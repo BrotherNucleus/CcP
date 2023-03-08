35e77afc6a35ef2bb4b1d8609cf8c02272d7920b
BrotherNucleus <jhuzarski@onet.pl>
2023-03-08 16:40:05 +0100

    Changes in project to .net 5.0

--- TPWJHMB/App.xaml.cs
+++ TPWJHMB/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace TPW_MB_JH
+namespace TPWJHMB
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- TPWJHMB/MainWindow.xaml.cs
+++ TPWJHMB/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TPW_MB_JH
+namespace TPWJHMB
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
