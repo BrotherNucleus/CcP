1e25e279f52b68a0bb3619f558c95e048060d979
Brother Nucleus <jhuzarski@onet.pl>
2023-04-11 18:31:23 +0200

    Created a scene class that represents what's on screen, it's height and width. Also minor changes to MainWindow.

---
+++ TPWClassLib/Scene.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWClassLib
+{
+    public class Scene
+    {
+        public Scene(int width, int height, int circleNumber, int radius) 
+        {
+            this.width = width;
+            this.height = height;
+            createCircleList(circleNumber, radius);
+        }
+
+        public int width { get; }
+
+        public int height { get; }
+
+        public int Enabled { get; set; }
+
+        public List<Circle> Circles { get; } = new List<Circle>();
+
+        public Circle createCircle(int radius)
+        {
+            Random random = new Random();
+            bool valid = true;
+            int x = radius, y = radius;
+            do
+            {
+                valid = true;
+                x = random.Next(radius, this.width - radius);
+                y = random.Next(radius, this.height - radius);
+                foreach (Circle b in this.Circles)
+                {
+                    double distance = Math.Sqrt(((b.X - x) * (b.X - x)) + ((b.Y - y) * (b.Y - y)));
+                    if (distance <= b.Radius + radius)
+                    {
+                        valid = false;
+                        break;
+                    };
+                }
+                if (!valid)
+                {
+                    continue;
+                };
+                valid = true;
+
+            } while (!valid);
+            return new Circle(radius, Color.AliceBlue, x, y);
+        }
+
+        public void createCircleList(int circleNumber, int radius)
+        {
+            Circles.Clear();
+            for (int i = 0; i < circleNumber; i++)
+            {
+                Circle circle = createCircle(radius);
+                this.Circles.Add(circle);
+            }
+        }
+    }
+}
---
+++ TPWTests/SceneTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPWClassLib;
+
+namespace TPWTests
+{
+    [TestClass]
+    public class SceneTests
+    {
+        [TestMethod]
+
+        public void SceneConstructorTest()
+        {
+            int testCircleNumber = 3;
+            int testWidth = 16, testHeight = 16;
+            int testRadius = 3;
+            Scene scene = new Scene(testWidth, testHeight, testCircleNumber, testRadius);
+
+            Assert.AreEqual(testCircleNumber, scene.Circles.Count);
+            Assert.AreEqual(testWidth, scene.width);
+            Assert.AreEqual(testHeight, scene.height);
+            Assert.AreEqual(testRadius, scene.Circles[0].Radius);
+        }
+
+        [TestMethod]
+
+        public void SceneCreateCricleTest()
+        {
+            int testCircleNumber = 1;
+            int testWidth = 16, testHeight = 16;
+            int testRadius = 3;
+            Scene scene = new Scene(testWidth, testHeight, testCircleNumber, testRadius);
+
+            Circle circle = scene.createCircle(testRadius);
+
+            Assert.IsTrue(circle != null);
+            Assert.IsTrue(circle.X > testRadius);
+            Assert.IsTrue(circle.X < scene.width - testRadius);
+            Assert.IsTrue(circle.Y > testRadius);
+            Assert.IsTrue(circle.Y < scene.height - testRadius);
+            Assert.AreEqual(Color.AliceBlue, circle.color);
+            Assert.AreEqual(testRadius, circle.Radius);
+        }
+
+        [TestMethod]
+
+        public void SceneCreateCircleListTest()
+        {
+            int testCircleNumber = 1;
+            int testWidth = 32, testHeight = 32;
+            int testRadius = 3;
+            Scene scene = new Scene(testWidth, testHeight, testCircleNumber, testRadius);
+
+            int newtestCircleNumber = 5;
+
+            scene.createCircleList(newtestCircleNumber, testRadius);
+
+            Assert.AreEqual(newtestCircleNumber, scene.Circles.Count);
+        }
+    }
+}
